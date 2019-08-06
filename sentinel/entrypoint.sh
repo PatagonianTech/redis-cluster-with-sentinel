33a452a3d890332dede1ae37d5827a0785dc90b9
Eduardo Cuomo <reduardo7@gmail.com>
2019-08-06 11:33:27 -0300

    Configuration

--- sentinel/entrypoint.sh
+++ sentinel/entrypoint.sh
@@ -3,8 +3,8 @@
 sed -i "s/\$REDIS_PORT/${REDIS_PORT:-6379}/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_PORT/${SENTINEL_PORT:-26379}/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_QUORUM/${SENTINEL_QUORUM:-2}/g" /etc/redis/sentinel.conf
-sed -i "s/\$SENTINEL_DOWN_AFTER/${SENTINEL_DOWN_AFTER:-30000}/g" /etc/redis/sentinel.conf
-sed -i "s/\$SENTINEL_FAILOVER/${SENTINEL_FAILOVER:-30000}/g" /etc/redis/sentinel.conf
+sed -i "s/\$SENTINEL_DOWN_AFTER/${SENTINEL_DOWN_AFTER:-3000}/g" /etc/redis/sentinel.conf
+sed -i "s/\$SENTINEL_FAILOVER/${SENTINEL_FAILOVER:-3000}/g" /etc/redis/sentinel.conf
 sed -i "s/\$REDIS_MASTER_NODE/${REDIS_MASTER_NODE:-redis-master}/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_MASTER_KEY/${SENTINEL_MASTER_KEY:-mymaster}/g" /etc/redis/sentinel.conf
 
